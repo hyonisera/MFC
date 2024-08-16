80fbd8a0a3df03886d47d96b78819011bb52c539
hyonisera <gydms3089@gmail.com>
2024-08-16 17:33:42 +0900

    first commit

---
+++ Message_practice/Message_practiceDlg.h
@@ -0,0 +1,39 @@
+﻿
+// Message_practiceDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMessagepracticeDlg 대화 상자
+class CMessagepracticeDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMessagepracticeDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MESSAGE_PRACTICE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+};
