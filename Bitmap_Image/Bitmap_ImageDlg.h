e8ec84ca58443dedd60f122307c7da5d4b74eb42
hyonisera <gydms3089@gmail.com>
2024-08-26 17:12:54 +0900

    이미지 업로드

---
+++ Bitmap_Image/Bitmap_ImageDlg.h
@@ -0,0 +1,37 @@
+﻿
+// Bitmap_ImageDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CBitmapImageDlg 대화 상자
+class CBitmapImageDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CBitmapImageDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BITMAP_IMAGE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_pic_control;
+	afx_msg void OnBnClickedButton();
+};
